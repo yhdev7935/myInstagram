ab046d18bc4b9ca8bd89f89941498d29c65bd648
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-12-31 03:38:20 +0900

    create: Main Navigation Bar

--- app/src/main/java/com/yhdev/myinstagram/MainActivity.kt
+++ app/src/main/java/com/yhdev/myinstagram/MainActivity.kt
@@ -2,10 +2,47 @@ package com.yhdev.myinstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.yhdev.myinstagram.navigation.AlarmFragment
+import com.yhdev.myinstagram.navigation.DetailViewFragment
+import com.yhdev.myinstagram.navigation.GridFragment
+import com.yhdev.myinstagram.navigation.UserFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true;
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true;
+            }
+            R.id.action_add_photo -> {
+                return true;
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true;
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true;
+            }
+        }
+        return false;
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener(this)
+
     }
 }
