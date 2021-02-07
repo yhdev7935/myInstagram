8052cc90236b21d8bbd995c07bddd94a80fc869c
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2021-02-08 04:23:48 +0900

    add Profile Image Upload, Follower, Following

--- app/src/main/java/com/yhdev/myinstagram/MainActivity.kt
+++ app/src/main/java/com/yhdev/myinstagram/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.yhdev.myinstagram
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -12,33 +13,44 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import com.yhdev.myinstagram.navigation.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         setToolbarDefault()
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.action_home -> {
                 var detailViewFragment = DetailViewFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
                 return true
             }
             R.id.action_search -> {
                 var gridFragment = GridFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment)
+                    .commit()
                 return true
             }
             R.id.action_add_photo -> {
-                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
                     startActivity(Intent(this, AddPhotoActivity::class.java))
                 }
                 return true
             }
             R.id.action_favorite_alarm -> {
                 var alarmFragment = AlarmFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment)
+                    .commit()
                 return true
             }
             R.id.action_account -> {
@@ -47,7 +59,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 var uid = FirebaseAuth.getInstance().currentUser?.uid
                 bundle.putString("destinationUid", uid)
                 userFragment.arguments = bundle
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment)
+                    .commit()
                 return true
             }
         }
@@ -63,10 +76,34 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener(this)
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnNavigationItemSelectedListener(
+            this
+        )
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
 
         // Set default screen
         findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == UserFragment.PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK) {
+            var imageUri = data?.data
+            var uid = FirebaseAuth.getInstance().uid
+            var storageRef =
+                FirebaseStorage.getInstance().reference.child("userProfileImages").child(uid!!)
+            storageRef.putFile(imageUri!!).continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+                return@continueWithTask storageRef.downloadUrl
+            }.addOnSuccessListener { uri ->
+                var map = HashMap<String, Any>()
+                map["image"] = uri.toString()
+                FirebaseFirestore.getInstance().collection("profileImages").document(uid).set(map)
+            }
+        }
     }
 }
--- app/src/main/java/com/yhdev/myinstagram/navigation/UserFragment.kt
+++ app/src/main/java/com/yhdev/myinstagram/navigation/UserFragment.kt
@@ -22,6 +22,7 @@ import com.yhdev.myinstagram.LoginActivity
 import com.yhdev.myinstagram.MainActivity
 import com.yhdev.myinstagram.R
 import com.yhdev.myinstagram.navigation.model.ContentDTO
+import com.yhdev.myinstagram.navigation.model.FollowDTO
 
 class UserFragment : Fragment() {
     var fragmentView: View? = null
@@ -29,6 +30,11 @@ class UserFragment : Fragment() {
     var uid: String? = null
     var auth: FirebaseAuth? = null
     var currentUserUid: String? = null
+
+    companion object {
+        var PICK_PROFILE_FROM_ALBUM = 10
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -59,17 +65,94 @@ class UserFragment : Fragment() {
             mainactivity?.findViewById<TextView>(R.id.toolbar_username)?.text =
                 arguments?.getString("userId")
             mainactivity?.findViewById<ImageView>(R.id.toolbar_btn_back)?.setOnClickListener {
-                mainactivity.findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
+                mainactivity.findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId =
+                    R.id.action_home
             }
             mainactivity.findViewById<ImageView>(R.id.toolbar_title_image).visibility = View.GONE
             mainactivity.findViewById<TextView>(R.id.toolbar_username).visibility = View.VISIBLE
             mainactivity.findViewById<ImageView>(R.id.toolbar_btn_back).visibility = View.VISIBLE
+            fragmentView?.findViewById<Button>(R.id.account_btn_follow_signout)
+                ?.setOnClickListener {
+                    requestFollow()
+                }
         }
         fragmentView?.findViewById<RecyclerView>(R.id.account_recyclerview)?.adapter =
             UserFragmentRecyclerViewAdapter()
         fragmentView?.findViewById<RecyclerView>(R.id.account_recyclerview)?.layoutManager =
             GridLayoutManager(activity!!, 3)
+
+        fragmentView?.findViewById<ImageView>(R.id.account_iv_profile)?.setOnClickListener {
+            var photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            activity?.startActivityForResult(photoPickerIntent, PICK_PROFILE_FROM_ALBUM)
+        }
+        getProfileImage()
         return fragmentView
+    }
+
+    fun requestFollow() {
+        var tsDocFollowing = firestore?.collection("users")?.document(currentUserUid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollowing!!).toObject(FollowDTO::class.java)
+            if (followDTO == null) {
+                // 팔로우 버튼을 눌렀는데, 나의 정보가 없을 때
+                followDTO = FollowDTO()
+                followDTO!!.followerCount = 1
+                followDTO!!.followers[uid!!] = true
+
+                transaction.set(tsDocFollowing, followDTO)
+                return@runTransaction
+            }
+
+            if (followDTO.followings.containsKey(uid)) {
+                // 이미 팔로우 했을 경우
+                followDTO?.followingCount = followDTO?.followingCount - 1
+                followDTO.followings.remove(uid)
+            } else {
+                followDTO?.followingCount = followDTO?.followingCount + 1
+                followDTO.followings[uid!!] = true
+            }
+
+            transaction.set(tsDocFollowing, followDTO)
+            return@runTransaction
+        }
+
+        var tsDocFollower = firestore?.collection("users")?.document(uid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollower!!).toObject(FollowDTO::class.java)
+            if (followDTO == null) {
+                followDTO = FollowDTO()
+                followDTO!!.followerCount = 1
+                followDTO!!.followers[currentUserUid!!] = true
+
+                transaction.set(tsDocFollower, followDTO!!)
+                return@runTransaction
+            }
+
+            if (followDTO!!.followers.containsKey(currentUserUid)) {
+                followDTO!!.followerCount = followDTO!!.followerCount - 1
+                followDTO!!.followers.remove(currentUserUid!!)
+            } else {
+                followDTO!!.followerCount + followDTO!!.followerCount + 1
+                followDTO!!.followers[currentUserUid!!] = true
+            }
+
+            transaction.set(tsDocFollower, followDTO!!)
+            return@runTransaction
+        }
+    }
+
+    fun getProfileImage() {
+        firestore?.collection("profileImages")?.document(uid!!)
+            ?.addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+                if (documentSnapshot == null) return@addSnapshotListener
+
+                if (documentSnapshot.data != null) {
+                    var url = documentSnapshot?.data!!["image"]
+                    Glide.with(activity!!).load(url).apply(RequestOptions().circleCrop())
+                        .into(fragmentView?.findViewById(R.id.account_iv_profile)!!)
+                }
+            }
     }
 
     inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
